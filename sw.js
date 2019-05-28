b33357c6bb66c0663838f18ba703c51a3effca09
Vlad Volkov <vlad@hiberbee.com>
2019-05-28 18:05:36 +0300

    Activated achives plugin

--- sw.js
+++ sw.js
@@ -7,7 +7,6 @@ const filesToCache = [
 ];
 const staticCacheName = "v1";
 self.addEventListener('install', event => {
-  console.log('Attempting to install service worker and cache static assets');
   event.waitUntil(
     caches.open(staticCacheName)
       .then(cache => {
@@ -17,15 +16,12 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
-  console.log('Fetch event for ', event.request.url);
   event.respondWith(
     caches.match(event.request)
       .then(response => {
         if (response) {
-          console.log('Found ', event.request.url, ' in cache');
           return response;
         }
-        console.log('Network request for ', event.request.url);
         return fetch(event.request).then(response => {
           // TODO 5 - Respond with custom 404 page
           return caches.open(staticCacheName).then(cache => {
