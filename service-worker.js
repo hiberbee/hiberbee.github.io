d2e25fb05ef73bb1d1e397a080768e6f39bf7b8e
Vladyslav Volkov <vlad@hiberbee.com>
2019-05-30 15:16:24 +0000

    Deploy Hiberbee/hiberbee.github.io to github.com/Hiberbee/hiberbee.github.io.git:master

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
             self.__precacheManifest = [{"url":"/index.html","revision":"0aa89df7cf1936d6bc69a5d2f6e98bf9"},{"url":"/404.html","revision":"fc9531ae3e2a623f2b695f567f39c755"},{"url":"/favicon-16x16.png","revision":"48ef8a84c99654ed87ddd65006395731"},{"url":"/favicon-32x32.png","revision":"a5c8df9c7eec1241c4ab33774a60d0b4"}];
             workbox.core.setCacheNameDetails({
     prefix: 'hiberbee',
-    suffix: 'v5',
+    suffix: 'v6',
     precache: 'precache',
     runtime: 'runtime-cache'
 });
