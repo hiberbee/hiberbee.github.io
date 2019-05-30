ac2709bd69ef45a592b3606301692d4561260236
Vladyslav Volkov <vlad@hiberbee.com>
2019-05-30 12:35:22 +0000

    Deploy Hiberbee/hiberbee.github.io to github.com/Hiberbee/hiberbee.github.io.git:master

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,32 @@
                         importScripts("/assets/js/workbox-v3.6.3/workbox-sw.js");
             workbox.setConfig({modulePathPrefix: "/assets/js/workbox-v3.6.3"});
 
-            self.__precacheManifest = [{"url":"/index.html","revision":"e365598acc769587c4231da24d3fd601"},{"url":"/404.html","revision":"b2743e519b6c163ff6a81980f407e411"},{"url":"/favicon-16x16.png","revision":"48ef8a84c99654ed87ddd65006395731"},{"url":"/favicon-32x32.png","revision":"a5c8df9c7eec1241c4ab33774a60d0b4"}];
+            self.__precacheManifest = [{"url":"/index.html","revision":"26a6b254682432168870a698f7a782ed"},{"url":"/404.html","revision":"7f0125aadb8dc262eb95921033c77522"},{"url":"/favicon-16x16.png","revision":"48ef8a84c99654ed87ddd65006395731"},{"url":"/favicon-32x32.png","revision":"a5c8df9c7eec1241c4ab33774a60d0b4"}];
             workbox.core.setCacheNameDetails({
-  prefix: 'hiberbee',
-  suffix: 'v1',
-  precache: 'precache',
-  runtime: 'runtime-cache'
+    prefix: 'hiberbee',
+    suffix: 'v2',
+    precache: 'precache',
+    runtime: 'runtime-cache'
 });
 
-// let Service Worker take control of pages ASAP
 workbox.skipWaiting();
 workbox.clientsClaim();
 
-// let Workbox handle our precache list
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
-// use `networkFirst` strategy for `*.html`, like all my posts
 workbox.routing.registerRoute(
-  /$/,
-  workbox.strategies.networkFirst()
+    /$/,
+    workbox.strategies.networkFirst()
 );
 
-// use `cacheFirst` strategy for images
 workbox.routing.registerRoute(
-  /assets\/(img)/,
-  workbox.strategies.cacheFirst()
+    /assets\/(img)/,
+    workbox.strategies.cacheFirst()
 );
 
-// third party files
 workbox.routing.registerRoute(
-  /^https?:\/\/(code.jquery.com|stackpath.bootstrapcdn.com|use.typekit.net|p.typekit.net|use.fontawesome.com)/,
-  workbox.strategies.staleWhileRevalidate()
+    /^https?:\/\/(code.jquery.com|stackpath.bootstrapcdn.com|use.typekit.net|p.typekit.net|use.fontawesome.com)/,
+    workbox.strategies.staleWhileRevalidate()
 );
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
