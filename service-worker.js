879843e91250aa4d4e4d357e5ba0af287f73ce14
Vlad Volkov <vlad@hiberbee.com>
2019-05-31 05:23:58 +0300

    Vcard, push notifications

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 workbox.core.setCacheNameDetails({
     prefix: 'hiberbee',
-    suffix: 'v7',
+    suffix: 'v8',
     precache: 'precache',
     runtime: 'runtime-cache'
 });
@@ -24,4 +24,5 @@ workbox.routing.registerRoute(
     /^https?:\/\/(use.typekit.net|p.typekit.net|use.fontawesome.com)/,
     workbox.strategies.staleWhileRevalidate()
 );
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
