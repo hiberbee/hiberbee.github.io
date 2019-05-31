56ee4d29503559a15bf5b70adfdee67d07d4b6ff
Vladyslav Volkov <vlad@hiberbee.com>
2019-05-31 02:35:48 +0000

    Deploy Hiberbee/hiberbee.github.io to github.com/Hiberbee/hiberbee.github.io.git:master

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
                         importScripts("/assets/js/workbox-v3.6.3/workbox-sw.js");
             workbox.setConfig({modulePathPrefix: "/assets/js/workbox-v3.6.3"});
 
-            self.__precacheManifest = [{"url":"/index.html","revision":"fe3232e404f9076ede9fb55653a23aba"},{"url":"/404.html","revision":"e68115ea394bf49dec6d021444e42305"},{"url":"/favicon-16x16.png","revision":"48ef8a84c99654ed87ddd65006395731"},{"url":"/favicon-32x32.png","revision":"a5c8df9c7eec1241c4ab33774a60d0b4"}];
+            self.__precacheManifest = [{"url":"/index.html","revision":"a48acfe7967160047b5f259c1308e616"},{"url":"/404.html","revision":"3c82c01fe4d29aa25e757725ba48ee6c"},{"url":"/favicon-16x16.png","revision":"48ef8a84c99654ed87ddd65006395731"},{"url":"/favicon-32x32.png","revision":"a5c8df9c7eec1241c4ab33774a60d0b4"}];
             workbox.core.setCacheNameDetails({
     prefix: 'hiberbee',
-    suffix: 'v7',
+    suffix: 'v8',
     precache: 'precache',
     runtime: 'runtime-cache'
 });
@@ -28,5 +28,6 @@ workbox.routing.registerRoute(
     /^https?:\/\/(use.typekit.net|p.typekit.net|use.fontawesome.com)/,
     workbox.strategies.staleWhileRevalidate()
 );
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
