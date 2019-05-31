941745daef813a9348bb22594821d80d8e5ec344
Vladyslav Volkov <vlad@hiberbee.com>
2019-05-31 03:53:20 +0000

    Deploy Hiberbee/hiberbee.github.io to github.com/Hiberbee/hiberbee.github.io.git:master

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
                         importScripts("/assets/js/workbox-v3.6.3/workbox-sw.js");
             workbox.setConfig({modulePathPrefix: "/assets/js/workbox-v3.6.3"});
 
-            self.__precacheManifest = [{"url":"/index.html","revision":"92e3bb613377b0e6de3cc3030bffc3a3"},{"url":"/404.html","revision":"08f066c421d943f507cffb40ce2473ff"},{"url":"/favicon-16x16.png","revision":"48ef8a84c99654ed87ddd65006395731"},{"url":"/favicon-32x32.png","revision":"a5c8df9c7eec1241c4ab33774a60d0b4"}];
+            self.__precacheManifest = [{"url":"/index.html","revision":"d92e059996646a7e2079db9c00e43fea"},{"url":"/404.html","revision":"e38de690cc0dc5c02c81b1f7cae7707c"},{"url":"/favicon-16x16.png","revision":"48ef8a84c99654ed87ddd65006395731"},{"url":"/favicon-32x32.png","revision":"a5c8df9c7eec1241c4ab33774a60d0b4"}];
             workbox.core.setCacheNameDetails({
     prefix: 'hiberbee',
-    suffix: 'v11',
+    suffix: 'v12',
     precache: 'precache',
     runtime: 'runtime-cache'
 });
