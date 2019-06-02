c206afaa07b5a0899914e5c5fd7f0b9db6774dc6
Vladyslav Volkov <vlad@hiberbee.com>
2019-06-02 21:02:48 +0000

    Deploy Hiberbee/hiberbee.github.io to github.com/Hiberbee/hiberbee.github.io.git:master

--- sw-register.js
+++ sw-register.js
@@ -1,5 +1,5 @@
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/service-worker.js?v=1559482434').then(function(reg) {
+            navigator.serviceWorker.register('/service-worker.js?v=1559509337').then(function(reg) {
                 reg.onupdatefound = function() {
                     var installingWorker = reg.installing;
                     installingWorker.onstatechange = function() {
