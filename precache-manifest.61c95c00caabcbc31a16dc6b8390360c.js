ba70b23eefdb19969ecaffb5a47ef99d71d8810a
Vlad Volkov <vlad@hiberbee.com>
2020-04-19 17:28:51 +0300

    Updates

---
+++ precache-manifest.61c95c00caabcbc31a16dc6b8390360c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7111dea084c8b6fbab2400631522da46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e72862ea4e4de5376c0",
+    "url": "/static/css/2.48a4df98.chunk.css"
+  },
+  {
+    "revision": "54c0be90c96dc77cf35e",
+    "url": "/static/css/main.8f7bf51c.chunk.css"
+  },
+  {
+    "revision": "9e72862ea4e4de5376c0",
+    "url": "/static/js/2.bcdd3f3f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bcdd3f3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54c0be90c96dc77cf35e",
+    "url": "/static/js/main.b6ec1c89.chunk.js"
+  },
+  {
+    "revision": "b74794bf1332804e2d95",
+    "url": "/static/js/runtime-main.654e96fd.js"
+  }
+]);
